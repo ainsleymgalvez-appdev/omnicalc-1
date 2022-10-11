c1f87ab819b60a4123a7b3c95b76d3a4e969f71b
Ainsley M Galvez <galvezainsley@gmail.com>
2022-10-11 16:20:24 +0000

    finished payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def calculate_square
     
-    @num = params.fetch("input_number").to_f
+    @num = params.fetch("squared").to_f
     @sqaure_of_num = @num ** 2
 
 
@@ -31,6 +31,51 @@ class ApplicationController < ActionController::Base
   def blank_random_form
 
     render ({:template => "calculation_templates/random_number.html.erb"})
+
+  end
+
+  def blank_payment_new
+
+    render ({:template => "calculation_templates/payment_number.html.erb"})
+  end
+
+  def calculate_payment
+    
+    @annual_rate = params.fetch("apr").to_f
+    @num_of_years = params.fetch("year").to_i
+    @loan_amt = params.fetch("principal").to_f
+
+    @num_of_payments = @num_of_years * 12
+    @annual_percent = @annual_rate / 100 
+    @monthly_rate = @annual_percent / 12
+
+    @numerator = @monthly_rate * @loan_amt
+    
+    @denominator_1 = @monthly_rate + 1
+    @denominator_2 = @num_of_payments * -1
+    @denominator_3 = @denominator_1 ** @denominator_2
+    @denominator_4 = 1 - @denominator_3
+
+    @payment = @numerator / @denominator_4
+
+
+    
+
+    render ({:template => "calculation_templates/payment_results.html.erb"})
+  end
+
+  def blank_square_root
+
+    render ({:template => "calculation_templates/square_root_number.html.erb"})
+
+  end
+
+  def calculate_square_root
+
+    @number = params.fetch("square_root").to_f
+    @square_root_of_num = Math.sqrt(@number)
+
+    render ({:template => "calculation_templates/square_root_results.html.erb"})
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
 
   get("/square/new", { :controller => "application", :action => "blank_square_form"})
 
-  get("/square_root/results", {:controller => "application", :action => "calculate_square" })
+  get("/square/results", {:controller => "application", :action => "calculate_square" })
 
   get("/random/results",  {:controller => "application", :action => "calculate_random"})
 
   get("/random/new", { :controller => "application", :action => "blank_random_form"})
 
-  # get("", { :controller => "", :action => ""})
+  get("payment/new", { :controller => "application", :action => "blank_payment_new"})
 
-  # get("", { :controller => "", :action => ""})
+  get("payment/results", { :controller => "application", :action => "calculate_payment"})
+
+  get("square_root/new", { :controller => "application", :action => "blank_square_root"})
+
+  get("square_root/results", { :controller => "application", :action => "calculate_square_root"})
+
 end
